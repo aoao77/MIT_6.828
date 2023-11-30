03b7221ba4bce62192510062f034187d1ea5b05c
company ubuntu yja <yangjiano@ubuntu_cmy.com>
2023-11-29 18:40:16 -0800

    finished mem_init of lab3

--- kern/pmap.c
+++ kern/pmap.c
@@ -321,7 +321,8 @@ page_init(void)
 		}
 		else if(i >= EXTPHYSMEM/PGSIZE) //[EXTPHYSMEM, ...).
 		{
-			if(i <= ((PADDR(pages) + npages * sizeof(struct PageInfo))/PGSIZE))
+			if(i <= ((PADDR(envs) + NENV * sizeof(struct Env))/PGSIZE))
+			// if(i <= ((PADDR(pages) + npages * sizeof(struct PageInfo))/PGSIZE))
 			{
 				pages[i].pp_ref = 1;
 				pages[i].pp_link = NULL;
@@ -669,7 +670,7 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 			user_mem_check_addr = i;
 			return -E_FAULT;
 		}
-		pte_t * pte_item = pgdir_walk(kern_pgdir, i, 0);
+		pte_t * pte_item = pgdir_walk(kern_pgdir, (void *)i, 0);
 		// pte not exist
 		if (pte_item == NULL)
 		{
